3378622ca2ebf2c9a277a94080a5d04ab91ba1d2
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2026-08-24 22:00:29 +0200

    Update critical.bat

--- critical.bat
+++ critical.bat
@@ -1,3 +1,3 @@
 :: Put this in the startup dir along with criticalntfy.ps1 except disable criticalntfy.ps1 in the task manager
 :: i have an "alias" for pwsh.bat in %systemroot% and the contents are: @powershell.exe %*
-conhost %systemroot%\pwsh.bat -Command ".\criticalntfy.ps1"
+start conhost %systemroot%\pwsh.bat -Command ".\criticalntfy.ps1"
